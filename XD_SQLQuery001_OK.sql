5e377e8067bccdf828def5020ea964c7dde647da
XeniaDay <47296253+XeniaDay@users.noreply.github.com>
2019-02-12 13:05:26 +0300

    Delete XD_SQLQuery001_OK.sql

--- XD_SQLQuery001_OK.sql
+++
@@ -1,4 +0,0 @@
---1. Все товары, в которых в название есть пометка urgent или название начинается с Animal
-
-SELECT * FROM [WideWorldImporters].[Warehouse].[StockItems]
-where StockItemName like '%urgent%'or StockItemName like 'Animal%'
